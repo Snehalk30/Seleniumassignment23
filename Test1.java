bfa63b861fb0437bd8950fdbffe106c4ff470457
hp <hp@host.docker.internal>
2023-10-17 13:41:01 +0800

    Pushing Test1.java File

---
+++ Test1.java
@@ -0,0 +1,24 @@
+
+public class Test1 {
+	
+ public static void main(String[] args) {
+
+		    int i = 10;
+		    int j = 7;
+
+		    int temp;
+
+		    temp = i; 
+		    i = j;    
+		    j = temp; 
+		    System.out.println("i = " + i);
+		    System.out.println("j = " + j);
+		 
+		    // Expected Result: i = 7 and j = 10
+
+		    
+		  }
+		}
+
+
+
