1f4c79572d0e6d8d8fb28d504b130970e934688b
hp <hp@host.docker.internal>
2023-10-29 12:11:23 +0800

    pushing palindrome file

---
+++ Palindrome.java
@@ -0,0 +1,31 @@
+
+	import java.util.Scanner;
+
+	public class Palindrome {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter a string: ");
+	        String input = scanner.nextLine();
+	        scanner.close();
+
+	        boolean isPalindrome = isPalindrome(input);
+
+	        if (isPalindrome) {
+	            System.out.println("Output: true )");
+	        } else {
+	            System.out.println("Output: false ");
+	        }
+	    }
+
+	    public static boolean isPalindrome(String str) {
+	        
+	       str = str.replaceAll("\\s", "").toLowerCase();
+
+	         String reversed = new StringBuilder(str).reverse().toString();
+
+	        return str.equals(reversed);
+	    }
+	}
+
+
+
